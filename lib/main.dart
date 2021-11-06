0e6cd3f296907cd1848f9183425674571017cb5c
hichem <ghenimi-hichem@hotmail.com>
2021-11-06 13:22:03 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,34 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("ghenimi"),
       ),
-      body: Container(
-        child: Text("hichem"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          // color: Colors.red,
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(colors: [
+                Colors.pink,
+                Colors.yellow,
+              ]),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red,
+                  blurRadius: 10,
+                )
+              ]),
+          child: Text(
+            "i am a box",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+        ),
       ),
     );
   }
