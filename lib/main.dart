a133dbdbeff5507d0fe4ec3f456609f8cc01c102
hichem <ghenimi-hichem@hotmail.com>
2021-11-08 10:48:53 +0100

    ch2_completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -18,33 +21,34 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("ghenimi"),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          // color: Colors.red,
-          width: 100,
-          height: 100,
-          clipBehavior: Clip.antiAlias,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
+      body: Container(
+        height: 200,
+        color: Colors.teal,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
               color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(colors: [
-                Colors.pink,
-                Colors.yellow,
-              ]),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.red,
-                  blurRadius: 10,
-                )
-              ]),
-          child: Text(
-            "i am a box",
-            style: TextStyle(
-              fontSize: 20,
+              alignment: Alignment.center,
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            )
+          ],
         ),
       ),
     );
